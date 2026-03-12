6ec738071807e9e98c097af779e24a9700b142cc
unknown <518arifulislam518@gmail.com>
2026-03-12 23:02:05 +0600

    added event

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const interviewList = [];
+const rejectedList = [];
+
+// step-1
 const allCards = document.getElementById("All-cards");
 
+// step-2
 const totalCountPart = document.getElementById("totalCount");
 const interviewCountPart = document.getElementById("interviewCount");
 const rejectedCountPart = document.getElementById("rejectedCount");
 
+interviewList.push({ name: 1 }, { name: 1 }, { name: 1 });
+
+// step-3
 function calculateCount() {
   totalCountPart.innerText = allCards.children.length;
+  interviewCountPart.innerText = interviewList.length;
+  rejectedCountPart.innerText = rejectedList.length;
 }
 calculateCount();
