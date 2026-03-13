fd02721921fe3ec94aa96c6d33d857717efb52a0
unknown <518arifulislam518@gmail.com>
2026-03-13 10:19:13 +0600

    added togoling button

--- script.js
+++ script.js
@@ -18,3 +18,23 @@ function calculateCount() {
   rejectedCountPart.innerText = rejectedList.length;
 }
 calculateCount();
+
+// step-4
+const allFilterBtn = document.getElementById("All-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+function clicked(id) {
+  console.log(id);
+  allFilterBtn.classList.remove("bg-blue-500", "text-white");
+  interviewFilterBtn.classList.remove("bg-blue-500", "text-white");
+  rejectedFilterBtn.classList.remove("bg-blue-500", "text-white");
+
+  allFilterBtn.classList.add("bg-white", "text-black");
+  interviewFilterBtn.classList.add("bg-white", "text-black");
+  rejectedFilterBtn.classList.add("bg-white", "text-black");
+
+  let selected = document.getElementById(id);
+  selected.classList.remove("bg-white", "text-black");
+  selected.classList.add("bg-blue-500", "text-white");
+}
