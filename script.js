9fcc1c1bd4430825acc26bb64ad6cf7d96102e8f
unknown <518arifulislam518@gmail.com>
2026-03-13 23:02:35 +0600

    added all button render

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const totalCountPart = document.getElementById("totalCount");
 const interviewCountPart = document.getElementById("interviewCount");
 const rejectedCountPart = document.getElementById("rejectedCount");
 
-interviewList.push({ name: 1 }, { name: 1 }, { name: 1 });
+// interviewList.push({ name: 1 }, { name: 1 }, { name: 1 });
 
 // step-3
 function calculateCount() {
@@ -19,7 +19,7 @@ function calculateCount() {
 }
 calculateCount();
 
-// step-4
+// step-4 ( button togoling)
 const allFilterBtn = document.getElementById("All-filter-btn");
 const interviewFilterBtn = document.getElementById("interview-filter-btn");
 const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
@@ -37,4 +37,144 @@ function clicked(id) {
   let selected = document.getElementById(id);
   selected.classList.remove("bg-white", "text-black");
   selected.classList.add("bg-blue-500", "text-white");
+
+  if (id == "interview-filter-btn") {
+    allCards.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+  } else if (id == "All-filter-btn") {
+    allCards.classList.remove("hidden");
+    filterSection.classList.add("hidden");
+  } else if (id == "rejected-filter-btn") {
+    allCards.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+  }
+}
+
+// step-5(using event deligation create an object for all information gatharing  and push the object within an array )
+const mainContainer = document.getElementById("main");
+mainContainer.addEventListener("click", function (event) {
+  // console.log(event.target.classList.contains("interview-btn"));
+  if (event.target.classList.contains("interview-btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+    const companyPart = parentNode.querySelector(".companyName").innerText;
+    const positionPart = parentNode.querySelector(".position").innerText;
+    const locationTypeSalaryPart = parentNode.querySelector(
+      ".locationTypeSalary",
+    ).innerText;
+    const flowChangePart = parentNode.querySelector(".flowChange").innerText;
+    const descriptionPart = parentNode.querySelector(".description").innerText;
+
+    parentNode.querySelector(".flowChange").innerText = "interview";
+
+    const objectInfo = {
+      companyPart,
+      positionPart,
+      locationTypeSalaryPart,
+      flowChangePart: "interview",
+      descriptionPart,
+    };
+
+    // console.log(objectInfo);
+
+    // step-6 (object push within array and check validation)
+    const validation = interviewList.find(
+      (item) => item.companyPart == objectInfo.companyPart,
+    );
+    if (!validation) {
+      interviewList.push(objectInfo);
+    }
+
+    rejectedList = rejectedList.filter(
+      (item) => item.companyPart != objectInfo.companyPart,
+    );
+    calculateCount();
+    // console.log(interviewList);
+
+    renderInterview();
+  } else if (event.target.classList.contains("rejected-btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+    const companyPart = parentNode.querySelector(".companyName").innerText;
+    const positionPart = parentNode.querySelector(".position").innerText;
+    const locationTypeSalaryPart = parentNode.querySelector(
+      ".locationTypeSalary",
+    ).innerText;
+    const flowChangePart = parentNode.querySelector(".flowChange").innerText;
+    const descriptionPart = parentNode.querySelector(".description").innerText;
+
+    parentNode.querySelector(".flowChange").innerText = "rejected";
+
+    const objectInfo = {
+      companyPart,
+      positionPart,
+      locationTypeSalaryPart,
+      flowChangePart: "rejected",
+      descriptionPart,
+    };
+
+    // console.log(objectInfo);
+
+    // step-6 (object push within array and check validation)
+    const validation = rejectedList.find(
+      (item) => item.companyPart == objectInfo.companyPart,
+    );
+    if (!validation) {
+      rejectedList.push(objectInfo);
+    }
+    calculateCount();
+    // console.log(interviewList);
+
+    renderRejected();
+  }
+});
+
+const filterSection = document.getElementById("filtered");
+function renderInterview() {
+  filterSection.innerHTML = "";
+  for (let interview of interviewList) {
+    let div = document.createElement("div");
+    div.className = "bg-white p-7 flex justify-between mb-5";
+    div.innerHTML = `
+    
+            <div class="space-y-4">
+                <h3 class=" companyName text-2xl font-bold">${interview.companyPart}</h3>
+            <p class="position text-gray-500">${interview.positionPart}</p>
+            <p class=" locationTypeSalary text-gray-500">${interview.locationTypeSalaryPart}</p>
+ <p class="flowChange bg-sky-100 w-[120px] px-4 py-2 ">Not Applied</p>
+ <p class="description text-gray-700">${interview.descriptionPart}</p>
+ <div>
+    <button class="border-2 border-green-400 px-4 py-2 rounded-[5px] text-green-500">Interview</button>
+    <button class="border-2 border-red-400 px-4 py-2 rounded-[5px] text-red-500">Rejected</button>
+ </div>
+            </div>
+            
+            <div><i class="fa-regular fa-trash-can"></i></div>
+
+   `;
+    filterSection.appendChild(div);
+  }
+}
+function renderRejected() {
+  filterSection.innerHTML = "";
+  for (let rejected of rejectedList) {
+    let div = document.createElement("div");
+    div.className = "bg-white p-7 flex justify-between mb-5";
+    div.innerHTML = `
+    
+            <div class="space-y-4">
+                <h3 class=" companyName text-2xl font-bold">${rejected.companyPart}</h3>
+            <p class="position text-gray-500">${rejected.positionPart}</p>
+            <p class=" locationTypeSalary text-gray-500">${rejected.locationTypeSalaryPart}</p>
+ <p class="flowChange bg-sky-100 w-[120px] px-4 py-2 ">Not Applied</p>
+ <p class="description text-gray-700">${rejected.descriptionPart}</p>
+ <div>
+    <button class="border-2 border-green-400 px-4 py-2 rounded-[5px] text-green-500">Interview</button>
+    <button class="border-2 border-red-400 px-4 py-2 rounded-[5px] text-red-500">Rejected</button>
+ </div>
+            </div>
+            
+            <div><i class="fa-regular fa-trash-can"></i></div>
+
+   `;
+    filterSection.appendChild(div);
+  }
 }
